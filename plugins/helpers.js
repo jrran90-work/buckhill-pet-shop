73774995aa342576eabc8d9e0be50712ad126ae5
jrran90-work <100705324+jrran90-work@users.noreply.github.com>
2022-03-04 23:18:37 +0800

    added global custom helper function for displaying authentication dialogs

--- plugins/helpers.js
+++ plugins/helpers.js
@@ -1,10 +1,25 @@
-export default ({app}, inject) => {
+export default ({ app, store }, inject) => {
   /**
    * Server API
    * @param string uri
    * @return string
    */
   inject('api', (uri) => {
-    return `${process.env.baseApi}/${uri}`;
-  });
-};
+    if (uri.charAt(0) === '/') {
+      uri = uri.substring(1)
+    }
+    return `${process.env.baseApi}/${uri}`
+  })
+
+  /**
+   * Auth Dialog (Login | Sign Up)
+   * @param string uri
+   * @return string
+   */
+  inject('showAuthDialog', (dialog, show) => {
+    store.dispatch('auth/showModal', {
+      show,
+      property: dialog,
+    })
+  })
+}
