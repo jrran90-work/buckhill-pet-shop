cf6c1a815206e43ff64aa753592dbf8efd517516
jrran90-work <100705324+jrran90-work@users.noreply.github.com>
2022-03-04 23:14:23 +0800

    Added API integration for user authentication

--- components/auth/Login.vue
+++ components/auth/Login.vue
@@ -1,15 +1,11 @@
 <template>
-  <v-dialog v-model="modal.login" width="540" @click:outside="showDialog('login', false)">
+  <v-dialog v-model="modal.login" width="540" :persistent="true">
     <v-card class="px-10 pt-8 pb-4">
-      <v-btn absolute top right icon @click="showDialog('login', false)">
+      <v-btn absolute top right icon @click="$showAuthDialog('login', false)">
         <v-icon>mdi-close</v-icon>
       </v-btn>
       <v-card-title class="mb-5">
-        <v-avatar
-          color="primary"
-          size="92"
-          class="white--text mx-auto"
-        >
+        <v-avatar color="primary" size="92" class="white--text mx-auto">
           <div>
             <v-icon color="white" size="52">mdi-paw</v-icon>
             <div class="caption font-weight-light">petson.</div>
@@ -18,18 +14,38 @@
       </v-card-title>
       <v-card-text class="text-h5 black--text text-center">Log in</v-card-text>
       <v-card-text>
-        <v-form>
-          <v-text-field label="Email Address*" outlined />
-          <v-text-field label="Password*" type="password" outlined />
-          <v-checkbox
-            label="Remember me"
-            class="mb-4"
-          ></v-checkbox>
-          <v-btn color="primary" block>Log in</v-btn>
+        <v-form ref="authForm" @submit.prevent="login">
+          <v-text-field
+            v-model="form.email"
+            :rules="rules.email"
+            label="Email Address*"
+            outlined
+          />
+          <v-text-field
+            v-model="form.password"
+            :rules="rules.password"
+            label="Password*"
+            type="password"
+            outlined
+          />
+          <v-checkbox v-model="form.remember" label="Remember me" class="mb-4"></v-checkbox>
+          <v-btn
+            type="submit"
+            color="primary"
+            :loading="form.loading"
+            :disabled="form.loading"
+            block
+            >Log in</v-btn
+          >
         </v-form>
         <v-card-actions class="d-flex justify-space-between mt-6">
           <NuxtLink to="/forgot" class="info--text">Forgot password?</NuxtLink>
-          <div class="info--text cursor-pointer" @click="showDialog('register', true)">Don't have an account? Sign Up</div>
+          <div
+            class="info--text cursor-pointer"
+            @click="$showAuthDialog('register', true)"
+          >
+            Don't have an account? Sign Up
+          </div>
         </v-card-actions>
       </v-card-text>
     </v-card>
@@ -38,20 +54,45 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { API } from '~/utils/api'
+import { rules } from '~/validations/auth'
 
 export default {
+  data() {
+    return {
+      rules,
+      form: {
+        email: '',
+        password: '',
+        remember: false,
+        loading: false,
+      },
+    }
+  },
   computed: {
     ...mapGetters({
-      modal: 'auth/modal'
-    })
-  }, 
+      modal: 'auth/modal',
+    }),
+  },
   methods: {
-    showDialog(dialog, show) {
-      this.$store.dispatch('auth/showModal', {
-        show,
-        property: dialog,
-      })
+    async login() {
+      if (!this.$refs.authForm.validate()) {
+        return
+      }
+
+      this.form.loading = true
+      try {
+        const { data } = await this.$axios.post(this.$api(API.USER_LOGIN), this.form)
+        this.$store.dispatch("auth/saveToken", {
+          token: data.data.token,
+          remember: this.form.remember
+        });
+        await this.$store.dispatch("auth/fetchUser")
+        this.$showAuthDialog('login', false)
+      } catch (e) {} finally {
+        this.form.loading = false
+      }
     },
-  }
+  },
 }
 </script>
