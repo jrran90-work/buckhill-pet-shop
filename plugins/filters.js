cf00aa1e8d971f9db25feeb8b86d532bf29abedc
jrran90-work <100705324+jrran90-work@users.noreply.github.com>
2022-03-06 01:09:46 +0800

    Created brand/category product page and added filters and pagination for products

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 
 export default () => {
+  /**
+   * capitalize
+   * @param string
+   * @return string
+   */
   Vue.filter('capitalize', function (value) {
+    if (!value) {
+      return
+    }
     const separateWord = value.toLowerCase().split(' ');
     for (let i = 0; i < separateWord.length; i++) {
        separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
@@ -10,9 +18,23 @@ export default () => {
     return separateWord.join(' ');
   })
 
+  /**
+   * initials
+   * @param string
+   * @return string
+   */
   Vue.filter('initials', function (name) {
     name = name.split(' ')
     return `${name[0].charAt(0)}${name[1] ? name[1].charAt(0) : ''}`;
 
+  })
+
+  /**
+   * excerpt
+   * @param string
+   * @return string
+   */
+  Vue.filter('excerpt', function (str, length) {
+    return str.substring(0,length) + "..."
   })
 }
